301dbb955263939ac0cef573ae3c2f47bbea957f
eltonCasa <elsbueno6@gmail.com>
2022-10-05 14:16:01 -0300

    Gerando Construtor na Model Aluno

---
+++ CadastroAluno.Test/AlunosControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using CadastroAluno.Contracts;
+using CadastroAluno.Controllers;
+using CadastroAluno.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CadastroAluno.Test
+{
+    public class AlunosControllerTest
+    {
+        Mock<IAlunoRepository> _repository;
+
+        public AlunosControllerTest()
+        {
+            _repository = new Mock<IAlunoRepository>();
+        }
+
+        [Fact]
+        public async void GetAluno_ExecutaAcao_RetornaOkAction()
+        {
+            //Arrange
+            AlunosController controller = new AlunosController(_repository.Object);
+
+
+
+            Aluno aluno1 = new Aluno("Bueno","T91", 5 );
+
+
+            _repository.Setup(repo => repo.GetAluno(1)).Returns(Task.FromResult(aluno1));
+
+            //Act
+            var consulta = await controller.Details(1);
+
+            //Assert
+            var cli = Assert.IsType<Aluno>(consulta);
+            //var Cliente = Assert.IsType<Cliente>((consulta.Result as OkObjectResult).Value);
+            //Assert.Equal(1 , listaClientes);
+
+        }
+
+    }
+}
--- CadastroAluno/Models/Aluno.cs
+++ CadastroAluno/Models/Aluno.cs
@@ -2,6 +2,13 @@
 {
     public class Aluno
     {
+        public Aluno(string nome, string turma, double media)
+        {
+            Nome = nome;
+            Turma = turma;
+            Media = media;
+        }
+
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Turma { get; set; }
